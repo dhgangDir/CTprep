2dbaff4d6b0dc76343a3def27bfd40c1d71b063a
dhgangDir <108117157+dhgangDir@users.noreply.github.com>
2025-08-08 16:22:03 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2104 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int max_n = 104;
+const int dy[] = {-1, 0, 1, 0};
+const int dx[] = {0, 1, 0, -1};
+int a[max_n][max_n], visited[max_n][max_n], n, m, y, x;
+int main(){
+    scanf("%d %d", &n, &m);
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            scanf("%1d", &a[i][j]);
+        }
+    }
+    queue<pair<int, int>> q;
+    visited[0][0] = 1;
+    q.push({0, 0});
+    while(q.size()){
+        tie(y, x) = q.front();
+        q.pop();
+        for(int i = 0; i < 4; i++){
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if(ny < 0 || ny >= n || nx < 0 || nx >= m || !a[ny][nx]) continue;
+            if(visited[ny][nx]) continue;
+            visited[ny][nx] = visited[y][x] + 1;
+            q.push({ny, nx});
+        }
+    }
+    cout << visited[n - 1][m - 1] << '\n';
+    return 0;
+}
