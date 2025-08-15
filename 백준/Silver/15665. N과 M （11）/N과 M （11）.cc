0955210898b41b79fa667c059704cb53c2dc5e75
dhgangDir <108117157+dhgangDir@users.noreply.github.com>
2025-08-15 13:12:46 +0900

    [Silver II] Title: N과 M (11), Time: 120 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+int arr[10], seq[10];
+void input() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+        cin >> seq[i];
+}
+
+void func(int size) {
+    if (size == m) {
+        for (int i = 0; i < m; i++) 
+            cout << arr[i] << ' ';
+        cout << '\n';
+        return ;
+    }
+    int tmp = 0;
+    for (int i = 0; i < n; i++) {
+        if (tmp != seq[i]) {
+            tmp = seq[i];
+            arr[size] = seq[i];
+            func(size + 1);
+        }
+    }
+}
+
+void solution() {
+    sort(seq, seq + n);
+    func(0);
+}
+
+void solve() {
+    input();
+    solution();
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+    return 0;
+}
