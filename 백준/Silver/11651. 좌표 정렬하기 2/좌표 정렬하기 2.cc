f1f881c45620aa2cc7a3871e16913b76c308d175
dhgangDir <108117157+dhgangDir@users.noreply.github.com>
2025-08-16 13:09:37 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 44 ms, Memory: 3584 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -1,31 +1,49 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
+#include <utility>
 using namespace std;
 
-vector<pair<int, int>> coord;
+#define MAX_LEN 100004
+
+pair<int, int> arr[MAX_LEN], tmp[MAX_LEN];
 int n;
 
 void input() {
-    int a, b;
     cin >> n;
-    while(n--) {
-        cin >> a >> b;
-        coord.push_back({a, b});
+    for (int i = 0; i < n; i++) {
+        int x, y;
+        cin >> x >> y;
+        arr[i] = {x, y};
     }
 }
 
-bool cmp(pair<int, int> a, pair<int, int> b) {
-    if (a.second == b.second) 
-        return a.first < b.first;
-    else 
-        return a.second < b.second;
+void merge(int st, int en) {
+    int mid = (st + en) / 2;
+    int lidx = st, ridx = mid;
+    for (int i = st; i < en; i++) {
+        if (lidx == mid) tmp[i] = arr[ridx++];
+        else if (ridx == en) tmp[i] = arr[lidx++];
+        else {
+            if (arr[lidx].second < arr[ridx].second) tmp[i] = arr[lidx++];
+            else if (arr[lidx].second == arr[ridx].second) {
+                if (arr[lidx].first <= arr[ridx].first) tmp[i] = arr[lidx++];
+                else tmp[i] = arr[ridx++];
+            } else tmp[i] = arr[ridx++];
+        }
+    }
+    for (int i = st; i < en; i++) arr[i] = tmp[i];
+}
+
+void merge_sort(int st, int en) {
+    if (en <= st + 1) return ;
+    int mid = (st + en) / 2;
+    merge_sort(st, mid);
+    merge_sort(mid, en);
+    merge(st, en);
 }
 
 void solution() {
-    sort(coord.begin(), coord.end(), cmp);
-    for (auto ab : coord) 
-        cout << ab.first << ' ' << ab.second << '\n';
+    merge_sort(0, n);
+    for (int i = 0; i < n; i++) cout << arr[i].first << " " << arr[i].second << '\n';
 }
 
 void solve() {
@@ -34,6 +52,9 @@ void solve() {
 }
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     solve();
     return 0;
 }
