e3ac3a92c6d1579989529cf2606328e60f145082
dhgangDir <108117157+dhgangDir@users.noreply.github.com>
2025-08-17 22:52:30 +0900

    [Silver I] Title: 스티커, Time: 80 ms, Memory: 3584 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.cc
+++ 백준/Silver/9465. 스티커/스티커.cc
@@ -1,36 +1,47 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
-#define MAX_ROW 2
-#define MAX_COL 100004
+#define MAX_NUM 100004
 
-int t, n, max_int;
-int stiker[MAX_ROW][MAX_COL];
-int dp[MAX_ROW][MAX_COL];
+int sticker[2][MAX_NUM];
+int dp[2][MAX_NUM];
+int t;
 
-int main() {
+void input() {
     cin >> t;
+}
 
-    while (t--) {
+void solution() {
+    while(t--) {
+        int n;
         cin >> n;
-        for (int i = 0; i < MAX_ROW; i++) {
-            for (int j = 0; j < n; j++) {
-                cin >> stiker[i][j];
-            }
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < n; j++) 
+                cin >> sticker[i][j];
         }
 
-        dp[0][0] = stiker[0][0];
-        dp[1][0] = stiker[1][0];
-        dp[0][1] = dp[1][0] + stiker[0][1];
-        dp[1][1] = dp[0][0] + stiker[1][1];
+        dp[0][0] = sticker[0][0]; 
+        dp[1][0] = sticker[1][0];
+        dp[0][1] = dp[1][0] + sticker[0][1];
+        dp[1][1] = dp[0][0] + sticker[1][1];
 
-        for (int j = 2; j < n; j++) {
-            dp[0][j] = max(dp[1][j - 1], dp[1][j - 2]) + stiker[0][j];
-            dp[1][j] = max(dp[0][j - 1], dp[0][j - 2]) + stiker[1][j];
+        for (int i = 2; i < n; i++) {
+            dp[0][i] = max(dp[1][i - 1], dp[1][i - 2]) + sticker[0][i];
+            dp[1][i] = max(dp[0][i - 1], dp[0][i - 2]) + sticker[1][i];
         }
-        
         cout << max(dp[0][n - 1], dp[1][n - 1]) << '\n';
     }
+}
+
+void solve() {
+    input();
+    solution();
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
     return 0;
 }
