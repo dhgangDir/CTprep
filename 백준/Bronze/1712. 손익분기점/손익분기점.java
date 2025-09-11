eb8af01c6df4a3c7ea20decbb009366b1ff6d453
dhgangDir <108117157+dhgangDir@users.noreply.github.com>
2025-09-11 17:06:48 +0900

    [Bronze II] Title: 손익분기점, Time: 100 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+
+        if (b >= c) System.out.println(-1);
+        else {
+            System.out.println( a / (c - b) + 1);
+        }
+    }
+}
