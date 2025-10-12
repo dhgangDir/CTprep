8048a945a2c48571e48db4e9edeedbca90abe992
dhgangDir <108117157+dhgangDir@users.noreply.github.com>
2025-10-12 21:31:21 +0900

    [Silver IV] Title: 문자열, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Silver/1120. 문자열/문자열.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String a = st.nextToken();
+        String b = st.nextToken();
+
+        int cntMin = 50;
+        for (int i = 0; i <= b.length() - a.length(); i++) {
+            int cnt = 0;
+            for (int j = 0; j < a.length(); j++) {
+                if (a.charAt(j) != b.charAt(i + j)) cnt++;
+            }
+            cntMin = Math.min(cntMin, cnt);
+        }
+        System.out.println(cntMin);
+    }
+}
