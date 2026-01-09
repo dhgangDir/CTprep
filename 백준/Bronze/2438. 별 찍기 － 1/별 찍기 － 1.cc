a72804f7cdcf6d11496298c9105340e2e85bb4da
dhgangDir <108117157+dhgangDir@users.noreply.github.com>
2026-01-09 13:48:51 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -1,17 +1,20 @@
 #include <iostream>
 using namespace std;
 
+void PrintStar(int n) {
+    if (!n) return ;
+    PrintStar(n - 1);
+    for (int i = 0; i < n; i++)
+        cout << "*";
+    cout << "\n";
+}
+
 int main() {
     int n;
-    cin >> n;
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (j <= i) cout << "*";
-            else break;
-        }
-        cout << "\n";
-    }
+    cin >> n;
+    
+    PrintStar(n);
 
     return 0;
 }
