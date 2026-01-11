1f97dc33553d4abd65d58219d13c410d34436d90
dhgangDir <108117157+dhgangDir@users.noreply.github.com>
2026-01-11 16:14:47 +0900

    [Bronze I] Title: 진법 변환 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.cc
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.cc
@@ -1,36 +1,21 @@
-#include <iostream>
-#include <stack>
+#include <bits/stdc++.h>
 using namespace std;
 
-stack<char> b_number_system;
-int number, n;
-
-void input() {
-    cin >> number >> n;
-}
-
-void solution() {
-    for (; number > 0; number /= n) {
-        int reminder = number % n;
-        if (reminder >= 0 && reminder <= 9) 
-            b_number_system.push(reminder + '0');
-        else 
-            b_number_system.push('A' + reminder - 10);
-    }
-
-    while (b_number_system.size()) {
-        cout << b_number_system.top();
-        b_number_system.pop();
-    }
-    cout << '\n';
-}
-
-void solve () {
-    input();
-    solution();
-}
-
 int main() {
-    solve();
-    return 0;   
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, b;
+    cin >> n >> b;
+
+    string s;
+    for (int i = n; i > 0; i /= b) {
+        if (i % b < 10) s.push_back(i % b + '0');
+        else s.push_back(i % b - 10 + 'A');
+    }
+    reverse(s.begin(), s.end());
+
+    cout << s << "\n";
+
+    return 0;
 }
