0faa7238145fea7a8adbfc752d67cd479212dcf8
dhgangDir <108117157+dhgangDir@users.noreply.github.com>
2026-01-11 16:00:00 +0900

    [Bronze II] Title: 진법 변환, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.cc
+++ 백준/Bronze/2745. 진법 변환/진법 변환.cc
@@ -1,35 +1,30 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-string b_number_system;
-int n, sum;
-
-void input() {
-    cin >> b_number_system >> n;
-}
-
-void solution() {
-    reverse(b_number_system.begin(), b_number_system.end());
-
-    int digit = 1;
-    for (int i = 0; i < b_number_system.size(); i++) {
-        if (b_number_system[i] >= '0' && b_number_system[i] <= '9')
-            sum = sum + (b_number_system[i] - '0') * digit;
-        else 
-            sum = sum + (b_number_system[i] - 'A' + 10) * digit;
-        digit = digit * n;
-    }
-
-    cout << sum << '\n';
-}
-
-void solve() {
-    input();
-    solution();
-}
+int cvrt[26] = {10,};
 
 int main() {
-    solve();
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    for (int i = 1; i < 26; i++) cvrt[i] = cvrt[i - 1] + 1;
+
+    string n;
+    int b;
+
+    cin >> n >> b;
+
+    reverse(n.begin(), n.end());
+    unsigned long long d = 1, ans = 0;
+    for (int i = 0; i < n.size(); i++) {
+        if (n[i] >= '0' && n[i] <= '9')
+            ans += (n[i] - '0') * d;
+        else
+            ans += cvrt[n[i] - 'A'] * d;
+        d *= b;
+    }
+
+    cout << ans << "\n";
+
     return 0;
 }
