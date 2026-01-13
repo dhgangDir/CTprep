e0d9ed58e114c3e3653a3fa194cbaa56695f54ee
dhgangDir <108117157+dhgangDir@users.noreply.github.com>
2026-01-13 16:57:39 +0900

    [Bronze III] Title: 약수 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.cc
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.cc
@@ -1,39 +1,23 @@
-#include <iostream>
-#include <cmath>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<int> divisors;
-int n, k;
-
-void input() {
-    cin >> n >> k;
-}
-
-void solution() {
-    int sqrt_n = (int)sqrt(n);
-    for (int i = 1; i <= sqrt_n; i++) {
-        if (!(n % i)) {
-            divisors.push_back(i);
-            if (i != n / i) 
-                divisors.push_back(n / i);
-        }
-    }
-    sort(divisors.begin(), divisors.end());
-    if (divisors.size() >= k) cout << divisors[k - 1] << '\n';
-    else cout << 0 << '\n';
-}
-
-void solve() {
-    input();
-    solution();
-}
-
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    solve();
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, k;
+    cin >> n >> k;
+
+    int cnt = 0, i = 1;
+
+    while (i <= n) {
+        if (!(n % i)) cnt++;
+        if (cnt == k) break;
+        i++;
+    }
+
+    if (cnt < k) cout << 0 << "\n";
+    else cout << i << "\n";
+
     return 0;
 }
