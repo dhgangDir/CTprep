764c442db7cc009bd060f04ced77788cb9ed4596
dhgangDir <108117157+dhgangDir@users.noreply.github.com>
2026-01-17 14:42:21 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 44 ms, Memory: 2808 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -1,60 +1,23 @@
-#include <iostream>
-#include <utility>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define MAX_LEN 100004
-
-pair<int, int> arr[MAX_LEN], tmp[MAX_LEN];
-int n;
-
-void input() {
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        int x, y;
-        cin >> x >> y;
-        arr[i] = {x, y};
-    }
-}
-
-void merge(int st, int en) {
-    int mid = (st + en) / 2;
-    int lidx = st, ridx = mid;
-    for (int i = st; i < en; i++) {
-        if (lidx == mid) tmp[i] = arr[ridx++];
-        else if (ridx == en) tmp[i] = arr[lidx++];
-        else {
-            if (arr[lidx].second < arr[ridx].second) tmp[i] = arr[lidx++];
-            else if (arr[lidx].second == arr[ridx].second) {
-                if (arr[lidx].first <= arr[ridx].first) tmp[i] = arr[lidx++];
-                else tmp[i] = arr[ridx++];
-            } else tmp[i] = arr[ridx++];
-        }
-    }
-    for (int i = st; i < en; i++) arr[i] = tmp[i];
-}
-
-void merge_sort(int st, int en) {
-    if (en <= st + 1) return ;
-    int mid = (st + en) / 2;
-    merge_sort(st, mid);
-    merge_sort(mid, en);
-    merge(st, en);
-}
-
-void solution() {
-    merge_sort(0, n);
-    for (int i = 0; i < n; i++) cout << arr[i].first << " " << arr[i].second << '\n';
-}
-
-void solve() {
-    input();
-    solution();
-}
+vector<pair<int, int>> v;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    solve();
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    v.resize(n);
+    for (int i = 0; i < n; ++i)
+        cin >> v[i].second >> v[i].first;
+
+    sort(v.begin(), v.end());
+
+    for (auto it : v)
+        cout << it.second << " " << it.first << "\n";
+
     return 0;
 }
