ad036d3de121742b744c1215618f9f0d7d2e2bed
dhgangDir <108117157+dhgangDir@users.noreply.github.com>
2026-01-17 14:19:18 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1536 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -1,37 +1,27 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define MAX_NUM 10004
-
-int num[MAX_NUM];
-int n, max_num;
-
-void input() {
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        int tmp;
-        cin >> tmp;
-        num[tmp]++;
-        max_num = max(max_num, tmp);
-    }
-}
-
-void solution() {
-    for (int i = 1; i <= max_num; i++) {
-        while (num[i]--)
-            cout << i << '\n';
-    }
-}
-
-void solve() {
-    input();
-    solution();
-}
+int check[10004];
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    solve();
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    int input;
+    for (int i = 0; i < n; ++i) {
+        cin >> input;
+        check[input]++;
+    }
+
+    for (int i = 1; i <= 10000; ++i) {
+        while (check[i]) {
+            cout << i << "\n";
+            --check[i];
+        }
+    }
+
     return 0;
 }
