fab986e9990900fef6c7063c90a602cea4ac7bea
dhgangDir <108117157+dhgangDir@users.noreply.github.com>
2026-01-18 16:46:24 +0900

    [Silver IV] Title: 가로수, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2485. 가로수/가로수.cc
+++ 백준/Silver/2485. 가로수/가로수.cc
@@ -14,15 +14,13 @@ int main() {
     for (int i = 0; i < n; ++i)
         cin >> v[i];
 
-    int gcdnum = gcd(v[1] - v[0], v[2] - v[1]);
-    for (int i = 2; i < n - 1; ++i)
-        gcdnum = gcd(gcdnum, v[i + 1] - v[i]);
+    int gcdDist = 0;
+    for (int i = 1; i < n; ++i) {
+        int dist = v[i] - v[i - 1];
+        gcdDist = gcd(gcdDist, dist);
+    }
 
-    long long cnt = 0;
-    for (int tree = v[0]; tree <= v[v.size() - 1]; tree += gcdnum)
-        cnt++;
-
-    cout << cnt - v.size() << "\n";
+    cout << (v.back() - v.front()) / gcdDist + 1 - n << "\n";
 
     return 0;
 }
