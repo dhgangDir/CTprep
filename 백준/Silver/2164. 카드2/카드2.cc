2e3892381a58d327c0dde3f1ca6391b3c3e9eb1e
dhgangDir <108117157+dhgangDir@users.noreply.github.com>
2026-01-20 17:02:08 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+queue<int> q;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; ++i) q.push(i);
+
+    while (q.size() > 1) {
+        q.pop();
+        q.push(q.front());
+        q.pop();
+    }
+
+    cout << q.front() << "\n";
+
+    return 0;
+}
