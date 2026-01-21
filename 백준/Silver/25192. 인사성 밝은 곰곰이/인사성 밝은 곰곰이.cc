4f3abca58e5dce3aa45a31e2661ff5b2256d1a4c
dhgangDir <108117157+dhgangDir@users.noreply.github.com>
2026-01-21 14:55:02 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 572 ms, Memory: 8312 KB -BaekjoonHub

---
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+unordered_set<string> st;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    int ans = 0;
+    while (n--) {
+        string s;
+        cin >> s;
+        if (s == "ENTER") {
+            ans += st.size();
+            st.clear();
+            continue;
+        }
+        st.insert(s);
+    }
+    ans += st.size();
+
+    cout << ans << "\n";
+
+    return 0;
+}
