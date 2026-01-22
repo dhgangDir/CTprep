7296ba4cd53a0f354256a816a4e3e68b8c6647e4
dhgangDir <108117157+dhgangDir@users.noreply.github.com>
2026-01-22 12:17:37 +0900

    [Silver III] Title: 칸토어 집합, Time: 0 ms, Memory: 2848 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.cc
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.cc
@@ -1,33 +1,28 @@
-#include <iostream>
-#include <cmath>
+#include <bits/stdc++.h>
 using namespace std;
 
-void func(int n, int splite_len, int s, int e, string& bar) {
-    int i;
-    splite_len = splite_len / 3;
-    if (n == 0) return ;
-
-    for (i = s + splite_len; i < e - splite_len; i++) 
-        bar[i] = ' ';
-    func(n - 1, splite_len, s, s + splite_len, bar);
-    func(n - 1, splite_len, e - splite_len, e, bar);
+void kantoa(string& s, int l, int r) {
+    if (r - l <= 1) return ;
+    int x = (r - l) / 3;
+    for (int i = x + l; i < x * 2 + l; ++i)
+        s[i] = ' ';
+    kantoa(s, l, x + l);
+    kantoa(s, x * 2 + l, r);
 }
 
 int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
     int n;
-    int i;
-    int split_len;
-    string bar;
-
     while (cin >> n) {
-        split_len = (int)pow(3, n);
-        for (i = 0; i < split_len; i++)
-            bar += '-';
-
-        func(n, split_len, 0, split_len, bar);
-
-        cout << bar << '\n';
-        bar = "";
+        string s;
+        int e = (int)pow(3, n);
+        s.resize(e);
+        for (int i = 0; i < e; ++i)
+            s[i] = '-';
+        kantoa(s, 0, e);
+        cout << s << "\n";
     }
 
     return 0;
