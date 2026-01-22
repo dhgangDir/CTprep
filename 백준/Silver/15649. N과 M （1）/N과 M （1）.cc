b8da1b6db82e2ef85a1633557f31e8a8bf83549f
dhgangDir <108117157+dhgangDir@users.noreply.github.com>
2026-01-22 14:47:10 +0900

    [Silver III] Title: N과 M (1), Time: 16 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,41 +1,36 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
+vector<bool> visited;
+vector<int> v;
 int n, m;
-int arr[10];
-bool isused[10];
 
-void input() {
-    cin >> n >> m;
-}
-
-void func(int size) {
-    if (size == m) {
-        for (int i = 0; i < m; i++) 
-            cout << arr[i] << ' ';
-        cout << '\n';
+void backtrack(int size) {
+    if (size >= m) {
+        for (auto& i : v)
+            cout << i << " ";
+        cout << "\n";
         return ;
     }
-
-    for (int i = 1; i <= n; i++) {
-        if (!isused[i]) {
-            arr[size] = i;
-            isused[i] = true;
-            func(size + 1);
-            isused[i] = false;
+    for (int i = 1; i <= n; ++i) {
+        if (visited[i]) {
+            v[size] = i;
+            visited[i] = false;
+            backtrack(size + 1);
+            visited[i] = true;
         }
     }
 }
 
-void solve() {
-    input();
-    func(0);
-}
-
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    solve();
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+    visited.resize(n + 1, true);
+    v.resize(m);
+
+    backtrack(0);
+
     return 0;
 }
