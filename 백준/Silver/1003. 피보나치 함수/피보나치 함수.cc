7741eb8fe14ace1c3c5058b0bb9321fbb35ff574
dhgangDir <108117157+dhgangDir@users.noreply.github.com>
2026-01-26 14:49:23 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dp[45][2] = {
+    {1, 0},
+    {0, 1},
+};
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    for (int i = 2; i <= 40; ++i) {
+        dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
+        dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
+    }
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+        cout << dp[n][0] << " " << dp[n][1] << "\n";
+    }
+
+    return 0;
+}
