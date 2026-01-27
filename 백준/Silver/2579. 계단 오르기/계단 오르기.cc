a7a872f533d652f7a1af7996c0aeb90a2c1d7335
dhgangDir <108117157+dhgangDir@users.noreply.github.com>
2026-01-27 15:49:25 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -1,28 +1,26 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define MAX_STAIR 304
-
-int stairs[MAX_STAIR], dp[MAX_STAIR];
-int n, max_score;
+const int MAX = 300;
+int stair[MAX + 4], dp[MAX + 4];
 
 int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
     cin >> n;
 
-    for (int i = 0; i < n; i++) {
-        cin >> stairs[i];
-    }
-    
-    dp[0] = stairs[0];
-    dp[1] = max(stairs[0] + stairs[1], stairs[1]);
-    dp[2] = max(stairs[0] + stairs[2], stairs[1] + stairs[2]);
+    for (int i = 1; i <= n; ++i)
+        cin >> stair[i];
 
-    for (int i = 3; i < n; i++) {
-        dp[i] = max(dp[i - 2] + stairs[i],
-                    dp[i - 3] + stairs[i - 1] + stairs[i]);
-    }
+    dp[1] = stair[1];
+    dp[2] = dp[1] + stair[2];
 
-    cout << dp[n - 1] << '\n';
+    for (int i = 3; i <= n; ++i)
+        dp[i] = max(stair[i - 1] + dp[i - 3], dp[i - 2]) + stair[i];
+
+    cout << dp[n] << "\n";
 
     return 0;
 }
