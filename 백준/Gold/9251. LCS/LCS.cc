aaacdc3ccee9f5c9a0bc4b15ce1bca6c42a9b246
dhgangDir <108117157+dhgangDir@users.noreply.github.com>
2026-01-29 13:06:13 +0900

    [Gold IV] Title: LCS, Time: 4 ms, Memory: 5964 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAX = 1000;
+int dp[MAX + 4][MAX + 4];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string a, b;
+    cin >> a >> b;
+
+    for (int i = 1; i <= a.size(); ++i) {
+        for (int j = 1; j <= b.size(); ++j) {
+            if (a[i - 1] == b[j - 1])
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+
+    cout << dp[a.size()][b.size()] << "\n";
+
+    return 0;
+}
