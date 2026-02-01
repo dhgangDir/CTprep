8ee7cf0cf490a38216cbe6cca53fba68bf63945b
dhgangDir <108117157+dhgangDir@users.noreply.github.com>
2026-02-01 18:56:34 +0900

    [Bronze II] Title: 이진수 합, Time: 56 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8741. 이진수 합/이진수 합.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; ++i)
+        cout << 1;
+    
+    for (int i = 0; i < n - 1; ++i)
+        cout << 0;
+    cout << "\n";
+
+    return 0;
+}
