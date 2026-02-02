857c20db4b12333ff45416103c42077b99e942bc
dhgangDir <108117157+dhgangDir@users.noreply.github.com>
2026-02-02 12:19:54 +0900

    [Silver IV] Title: 동전 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> v(n);
+    for (int i = 0; i < n; ++i)
+        cin >> v[i];
+
+    int ans = 0;
+    for (int i = n - 1; i >= 0; --i) {
+        if (k % v[i] != k) {
+            ans += k / v[i];
+            k %= v[i];
+        }
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
