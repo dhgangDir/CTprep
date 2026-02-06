33aec218451f30cdfd752d6fd54f06eb6f34eba8
dhgangDir <108117157+dhgangDir@users.noreply.github.com>
2026-02-06 13:15:48 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 296 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,7 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> v;
+const int MAX = 500000;
+int cards[MAX + 4];
+
+void func(int mid, int size) {
+    int i = mid;
+    while (i > 0 && cards[i - 1] == cards[mid])
+        i--;
+
+    int j = mid;
+    while (j < size && cards[j + 1] == cards[mid])
+        j++;
+    cout << j - i + 1 << " ";
+}
 
 int main() {
     ios::sync_with_stdio(0);
@@ -10,21 +22,19 @@ int main() {
     int n;
     cin >> n;
 
-    v.resize(n);
     for (int i = 0; i < n; ++i)
-        cin >> v[i];
+        cin >> cards[i];
 
-    sort(v.begin(), v.end());
+    sort(cards, cards + n);
 
     int m;
     cin >> m;
     while (m--) {
-        int target;
-        cin >> target;
-        cout << upper_bound(v.begin(), v.end(), target) -
-            lower_bound(v.begin(), v.end(), target) << " ";
+        int num;
+        cin >> num;
+
+        cout << upper_bound(cards, cards + n, num)
+            - lower_bound(cards, cards + n, num) << " ";
     }
     cout << "\n";
-
-    return 0;
 }
