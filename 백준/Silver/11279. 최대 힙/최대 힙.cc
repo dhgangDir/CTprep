b19656b3f0fafd2f0ec39e38892ae3995c052ecc
dhgangDir <108117157+dhgangDir@users.noreply.github.com>
2026-02-09 12:38:28 +0900

    [Silver II] Title: 최대 힙, Time: 8 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+priority_queue<int> pq;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    while (n--) {
+        int num;
+        cin >> num;
+        if (num == 0) {
+            if (!pq.empty()) {
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+            else cout << 0 << "\n";
+        }
+        else
+            pq.push(num);
+    }
+
+    return 0;
+}
