5b066405940a3c530684594dbb19c0bb6669589a
dhgangDir <108117157+dhgangDir@users.noreply.github.com>
2026-02-10 14:06:51 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 80 ms, Memory: 10928 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAX = 100000;
+vector<int> adj[MAX + 4];
+int visited[MAX + 4], cnt = 1;
+
+void dfs(int r) {
+    visited[r] = cnt++;
+    for (const auto& x : adj[r])
+        if (visited[x] == 0)
+            dfs(x);
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, m, r;
+    cin >> n >> m >> r;
+
+    while (m--) {
+        int v, e;
+        cin >> v >> e;
+        adj[v].push_back(e);
+        adj[e].push_back(v);
+    }
+
+    for (int i = 1; i <= n; ++i)
+        sort(adj[i].begin(), adj[i].end());
+
+    dfs(r);
+
+    for (int i = 1; i <= n; ++i)
+        cout << visited[i] << "\n";
+
+    return 0;
+}
